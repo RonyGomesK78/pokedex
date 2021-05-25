e12f1604c046696b4f7c370f93e0c5f9a5e5ae6e
RonyGomesK78 <gromilton7@gmail.com>
2021-05-25 03:25:50 -0100

    add css style on card hover

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,12 +6,13 @@ export const PokemonTypes = [
     { color: "#62626d", type: "bug" },
     { color: "#b3b4b8", type: "normal"},
     { color: "#e4e972", type: "electric"},
-    { color: "#d3b284", type: "ground"},
-    { color: "#e566a3", type: "fairy"},
-    { color: "#890400", type: "fighting"},
-    { color: "#edb1ff", type: "psychic"},
-    { color: "#50463d", type: "rock"},
-    { color: "#5bdbd7", type: "ghost"},
-    { color: "#d13117", type: "dragon"},
+    { color: "#d3b284", type: "ground" },
+    { color: "#e566a3", type: "fairy" },
+    { color: "#890400", type: "fighting" },
+    { color: "#edb1ff", type: "psychic" },
+    { color: "#50463d", type: "rock" },
+    { color: "#5bdbd7", type: "ghost" },
+    { color: "#d13117", type: "dragon" },
+    { color: "#dbf4f7", type: "ice" }
 
 ]
